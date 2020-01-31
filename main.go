6b870553ca042aa912b85b9b6278b9d0c921e4ef
gabbvincent <vincentgabb@gmail.com>
2020-01-31 03:42:40 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,14 @@
+// Programmer name: Vincent Gabb
+// Date completed:  1/30/2020
+// Description: 1.2.1 Lab Part 1
+
+package main
+
+import "fmt" 
+
+func main() {
+	fmt.Println("Hello world!")
+	fmt.Println("My name is Vincent Gabb, and my major is Computer Science.")
+	fmt.Println("This is my Third semester at Butte College.")
+	fmt.Println("One of my favorite pastimes is Rock CLimbing.")
+}
